d120c67a8ed93c773e3500f890d36a73874a8613
Brandon Little <brandonlittle1999@hotmail.com>
2020-04-08 10:44:53 +0100

    Added the reset button to JavaScript, CSS and HTML.

--- scripts.js
+++ scripts.js
@@ -17,11 +17,30 @@ function createDiv() {
 let gridCell = document.getElementsByClassName("gridCell");
 
 for (i = 0; i < gridCell.length; i++) {
-    gridCell[i].id = [i];
     gridCell[i].addEventListener("mouseover", placeColour);
 }
 
 //Adding the colour as the user hovers over the various cell tiles.
+let colour;
+
 function placeColour(){
+    randomColour();
     this.style.backgroundColor = colour;    
+}
+
+function randomColour(){
+    let red = Math.floor(Math.random()* 255);
+    let blue = Math.floor(Math.random()* 255);
+    let green = Math.floor(Math.random()* 255);
+    colour = `RGB( ${red}, ${blue}, ${green})`; // Here I use template literals so I can easily include the different colours in my string.
+}
+
+//Reset button to clear the board of the colours when the grid is filled or the user wants to start over
+
+document.getElementById("resetButton").addEventListener("click", reset);
+
+function reset(){
+    for (i = 0; i < gridCell.length; i++) {
+        gridCell[i].style.backgroundColor = "RGB(255, 255, 255)";
+    }
 }
